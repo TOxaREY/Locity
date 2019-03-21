eaf2db961ac92188c984d1e8cf5797af36bdc9c1
TOxaREY <delta@toxarey.xyz>
2019-03-21 20:22:44 +0300

    using coredata and realm

--- решения/letter&data/letter&data/ViewController.swift
+++ решения/letter&data/letter&data/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import Foundation
+import CoreData
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelUp: UILabel!
+    
     
     var i = 0
     var rand = 0
@@ -45,6 +49,66 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        let country = Country()
+//        country.en = "Australia"
+//        country.ru = "Австралия"
+//        country.iso = "AU"
+//
+//        let city1 = City()
+//        city1.name = "Sydney"
+//        city1.ru = "Сидней"
+//        city1.coor = 45.56
+//        country.addToCitys(city1)
+//
+//        let city2 = City()
+//        city2.name = "Melbourne"
+//        city2.ru = "Мельбурн"
+//        city2.coor = 56.89
+//        country.addToCitys(city2)
+//
+//        let city3 = City()
+//        city3.name = "Perth"
+//        city3.ru = "Перт"
+//        city3.coor = 36.45
+//        country.addToCitys(city3)
+        
+//                let country = Country()
+//                country.en = "New Zealand"
+//                country.ru = "Новая Зеландия"
+//                country.iso = "NZ"
+//        
+//                let city1 = City()
+//                city1.name = "Auckland"
+//                city1.ru = "Окленд"
+//                city1.coor = 87.55
+//        country.addToCitys(city1)
+//        
+//                let city2 = City()
+//                city2.name = "Wellington"
+//                city2.ru = "Веллингтон"
+//                city2.coor = 16.33
+//        country.addToCitys(city2)
+//
+//                let city3 = City()
+//                city3.name = "Christchurch"
+//                city3.ru = "Крайстчерч"
+//                city3.coor = 25.74
+//        country.addToCitys(city3)
+
+//        CoreDataManager.instance.saveContext()
+
+//        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Country")
+//        do {
+//            let results = try CoreDataManager.instance.persistentContainer.viewContext.fetch(request)
+//            for result in results as! [Country] {
+//                print(result)
+//            }
+//
+//        } catch {
+//
+//            print("Failed")
+//        }
+
     }
     
     
---
+++ решения/weq/Pods/Target Support Files/Pods-weq/Pods-weq-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weq : NSObject
+@end
+@implementation PodsDummy_Pods_weq
+@end
