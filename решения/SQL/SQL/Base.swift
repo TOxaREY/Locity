3062e151d821a3d99aef936957a8e7e49c993eec
TOxaREY <delta@toxarey.xyz>
2019-03-28 22:17:51 +0300

    sqlite

--- решения/SQL/SQL/Base.swift
+++ решения/SQL/SQL/Base.swift
@@ -1,25 +1,9 @@
 //
 //  Base.swift
+//  SQL
 //
-//
-//  Created by REYNIKOV ANTON on 22/03/2019.
+//  Created by REYNIKOV ANTON on 28/03/2019.
+//  Copyright © 2019 REYNIKOV ANTON. All rights reserved.
 //
 
-//import Foundation
-//import SQLite
-//class Database {
-//    static let shared = Database()
-//    public let db: Connection?
-//    private init(){
-//        do {
-//            let dbPath = Bundle.main.path(forResource: "world", ofType: "db")!
-//            db = try Connection(dbPath, readonly: true)
-//            print("Connect")
-//
-//        } catch {
-//            db = nil
-//            let nserror = error as NSError
-//            print ("Cannot connect to Database. Error is: \(nserror), \(nserror.userInfo)")
-//        }
-//    }
-//}
+import Foundation
